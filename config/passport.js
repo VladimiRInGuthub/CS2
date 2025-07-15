3382e231237b332beb2ef52948d4b2488dcf0294
Vl3dimiR <96626150+VladimiRInGuthub@users.noreply.github.com>
2025-07-15 19:01:09 +0200

    Fix Steam login redirect

--- config/passport.js
+++ config/passport.js
@@ -24,26 +24,28 @@ passport.use(new SteamStrategy({
     user = await new User({
       steamId,
       username: profile.displayName,
-      avatar: profile.photos[2].value
+      avatar: profile?.photos?.[2]?.value || ''
     }).save();
   }
   done(null, user);
 }));
 
-// 🟢 Google
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/auth/google/callback'
-}, async (accessToken, refreshToken, profile, done) => {
-  const googleId = profile.id;
-  let user = await User.findOne({ googleId });
-  if (!user) {
-    user = await new User({
-      googleId,
-      username: profile.displayName,
-      avatar: profile.photos[0].value
-    }).save();
-  }
-  done(null, user);
-}));
+// 🟢 Google (optionnel)
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:5000/auth/google/callback'
+  }, async (accessToken, refreshToken, profile, done) => {
+    const googleId = profile.id;
+    let user = await User.findOne({ googleId });
+    if (!user) {
+      user = await new User({
+        googleId,
+        username: profile.displayName,
+        avatar: profile.photos[0].value
+      }).save();
+    }
+    done(null, user);
+  }));
+}
